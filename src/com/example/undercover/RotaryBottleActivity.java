2c4908b13d128351c1c3f35ee32205881b9520ae
lijincheng <tjpuljc@163.com>
2013-08-06 00:31:05 +0800

    改善转酒瓶

--- src/com/example/undercover/RotaryBottleActivity.java
+++ src/com/example/undercover/RotaryBottleActivity.java
@@ -2,12 +2,15 @@ package com.example.undercover;
 
 import java.util.Random;
 
+import android.R.interpolator;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
+import android.view.animation.Interpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -42,6 +45,21 @@ public class RotaryBottleActivity extends Activity{
 			}
 		});	
 		
+		
+		
+		final Button returnBtn = (Button)findViewById(R.id.rotarybottle_returnBtn);
+		returnBtn.setClickable(false);
+		returnBtn.setOnClickListener(new Button.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent	retrunIntent = new Intent();
+				retrunIntent.setClass(RotaryBottleActivity.this,home_page.class);
+				startActivity(retrunIntent);
+			}
+		});	
+		
 		ViewTreeObserver vto2 = bottle.getViewTreeObserver();
 		vto2.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 			
@@ -49,6 +67,7 @@ public class RotaryBottleActivity extends Activity{
 			public void onGlobalLayout() {
 				// TODO Auto-generated method stub
 				bottle.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+//				bottle.getViewTreeObserver().removeOnGlobalLayoutListener(this);
 				bottleWidth = bottle.getWidth();
 				bottleHeight = bottle.getHeight();
 				
@@ -61,22 +80,23 @@ public class RotaryBottleActivity extends Activity{
 	
 	private float getDegrees()
 	{
-		Random random = new Random(System.currentTimeMillis());
-		float ranFloat =  random.nextFloat()*1000;
-		while(ranFloat<720)
-		{
-			ranFloat = random.nextFloat()*1000;
-		}
-		
-		return ranFloat;
+		return (float)Math.round(Math.random()*360);
 	}
 	
 	private Animation getAnimation(float fromDegrees,float toDegrees)
 	{
 		AnimationSet as=new AnimationSet(true);  
-		RotateAnimation rt = new RotateAnimation(fromDegrees,toDegrees,bottleWidth/2,bottleHeight/2);
+		RotateAnimation rt = new RotateAnimation(fromDegrees,2880+toDegrees,bottleWidth/2,bottleHeight/2);
 		rt.setDuration(6000);
 		rt.setFillAfter(true);
+		rt.setInterpolator(new Interpolator() {
+			
+			@Override
+			public float getInterpolation(float arg0) {
+				// TODO Auto-generated method stub
+				return interpolator.accelerate_decelerate;
+			}
+		});
 		as.addAnimation(rt);
 		
 		fromDe = toDegrees%360;
