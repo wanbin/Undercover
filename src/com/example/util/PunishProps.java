2b704b45cd8b62784873efc4402d6d03734d4583
wanbin <wanbin89731@163.com>
2013-10-12 03:26:16 +0800

    有胆量就问LOG

--- src/com/example/util/PunishProps.java
+++ src/com/example/util/PunishProps.java
@@ -5,13 +5,14 @@ import java.util.ResourceBundle;
 public class PunishProps {
 	/** 绕口令的数目 */
 	private static int raokouling = 55;
-	//加载punish.properties文件
+	// 加载punish.properties文件
 	private static final ResourceBundle resourceBundle = ResourceBundle.getBundle("punish");
-	//加载question.properties文件
+	// 加载question.properties文件
 	private static final ResourceBundle questionBundle = ResourceBundle.getBundle("question");
 	
 	/**
 	 * 获取惩罚条目。
+	 * 
 	 * @param str
 	 * @return
 	 */
@@ -21,6 +22,7 @@ public class PunishProps {
 	
 	/**
 	 * 获得较为复杂的问题
+	 * 
 	 * @param str
 	 * @return
 	 */
@@ -30,6 +32,7 @@ public class PunishProps {
 	
 	/**
 	 * 获得较为简单的问题
+	 * 
 	 * @param str
 	 * @return
 	 */
@@ -39,18 +42,20 @@ public class PunishProps {
 	
 	/**
 	 * 获得绕口令
+	 * 
 	 * @param str
 	 * @return
 	 */
 	public static String getRaoKouLing(int str){
 		return questionBundle.getString("raokouling_"+str+"");
 	}
+	
 	/**
 	 * 获得一条绕口令
+	 * 
 	 * @return
 	 */
 	public static String getRaoKouLing(){
-		
 		return getRaoKouLing((int) Math.floor(Math.random() * raokouling));
 	}
 }
