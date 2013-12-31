bebf3e31edf1849cf9d3433b8b7bb18bc164fe35
jlcao <cjl_2029@163.com>
2013-12-31 15:05:47 +0800

    优化了用户贡献

--- src/com/example/undercover/userContribute.java
+++ src/com/example/undercover/userContribute.java
@@ -1,11 +1,59 @@
 package com.example.undercover;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class userContribute extends BaseActivity {
+	private Button AddTureBt;
+	private Button AddAdvenBt;
+	private Button userConSend;
+	private EditText userConTextField; 
+	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_usercontribute);
+		
+		AddTureBt = (Button)findViewById(R.id.addTruth);
+		AddAdvenBt = (Button) findViewById(R.id.addAdventure);
+		userConSend = (Button) findViewById(R.id.userConSend);
+		userConTextField = (EditText) findViewById(R.id.userConTextField);
+		
+		userConSend.setVisibility(View.INVISIBLE);
+		userConTextField.setVisibility(View.INVISIBLE);
+		
+		//添加真心话按钮
+		AddTureBt.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				userConSend.setVisibility(View.VISIBLE);
+				userConTextField.setVisibility(View.VISIBLE);
+				AddAdvenBt.setVisibility(View.INVISIBLE);
+				AddTureBt.setVisibility(View.INVISIBLE);
+			}
+		});
+		//添加大冒险按钮
+		AddAdvenBt.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				userConSend.setVisibility(View.VISIBLE);
+				userConTextField.setVisibility(View.VISIBLE);
+				AddAdvenBt.setVisibility(View.INVISIBLE);
+				AddTureBt.setVisibility(View.INVISIBLE);
+				
+			}
+		});
+		//发送用户编写的真心话大冒险
+		userConSend.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
 	}
 	
 	/**
