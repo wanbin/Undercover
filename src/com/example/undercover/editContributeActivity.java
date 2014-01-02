48ea6d58eb5d2db9494e0b24d2b5143d6fbc36a7
jlcao <cjl_2029@163.com>
2014-01-02 23:08:50 +0800

    添加发送真心话大冒险

--- src/com/example/undercover/editContributeActivity.java
+++ src/com/example/undercover/editContributeActivity.java
@@ -1,24 +1,28 @@
 package com.example.undercover;
 
+import org.json.JSONObject;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class editContributeActivity extends BaseActivity {
 	private Button send;
 	private Button cancle;
+	private EditText textCotent;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_editcontribute);
 		
 		send = (Button) findViewById(R.id.send);
 		cancle = (Button) findViewById(R.id.cancle);
-		
+		textCotent = (EditText) findViewById(R.id.editText1);
 		send.setOnClickListener(new Button.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				
+				sendPublish(textCotent.getText().toString(),1);
 				
 			}
 		});
@@ -32,5 +36,17 @@ public class editContributeActivity extends BaseActivity {
 		});
 
 	}
-
+	/**
+	 * 发布新闻
+	 */
+	protected void sendPublish(String content, int type) {
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("content", content);
+			obj.put("type", type);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		super.getHttpRequest(obj, ConstantControl.SEND_PUBLISH_ALL);
+	}
 }
