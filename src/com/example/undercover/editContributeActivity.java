b0667fbbb7e8a14cc0482ae2a5c71a17d7504fcb
jlcao <cjl_2029@163.com>
2014-01-07 22:38:15 +0800

    更新用户贡献编辑界面

--- src/com/example/undercover/editContributeActivity.java
+++ src/com/example/undercover/editContributeActivity.java
@@ -8,24 +8,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class editContributeActivity extends BaseActivity {
-	private Button send;
-	private Button cancle;
+	private ImageView send;
+	private ImageView cancle;
 	private EditText textCotent;
 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_editcontribute);
 
-		send = (Button) findViewById(R.id.send);
-		cancle = (Button) findViewById(R.id.cancle);
+		send = (ImageView) findViewById(R.id.send);
+		cancle = (ImageView) findViewById(R.id.cancle);
 		textCotent = (EditText) findViewById(R.id.editText1);
 		send.setOnClickListener(new Button.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				sendPublish();
+				finish();
 			}
 		});
 
